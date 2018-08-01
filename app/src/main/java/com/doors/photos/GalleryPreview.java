c63439f181dc555728a7d8ac8c3368214132acb2
Domi <domi04151309business@gmail.com>
2018-08-01 22:05:53 +0200

    Changed some things

--- app/src/main/java/com/doors/photos/GalleryPreview.java
+++ app/src/main/java/com/doors/photos/GalleryPreview.java
@@ -2,24 +2,24 @@ package com.doors.photos;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
-
 import com.bumptech.glide.Glide;
-
 import java.io.File;
 
-/**
- * Created by SHAJIB on 25/12/2015.
- */
 public class GalleryPreview extends AppCompatActivity {
 
+    private View mContentView;
     ImageView GalleryPreviewImg;
     String path;
+
+    Boolean showUI = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.gallery_preview);
         Intent intent = getIntent();
         path = intent.getStringExtra("path");
@@ -27,5 +27,35 @@ public class GalleryPreview extends AppCompatActivity {
         Glide.with(GalleryPreview.this)
                 .load(new File(path)) // Uri of the picture
                 .into(GalleryPreviewImg);
+
+        mContentView = findViewById(R.id.relative_container);
+        mContentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (showUI) {
+                    hide();
+                    showUI = false;
+                }else {
+                    show();
+                    showUI = false;
+                }
+            }
+        });
+    }
+
+    private void hide(){
+        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    private void show(){
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.show();
+        }
     }
 }
--- app/src/main/java/com/doors/photos/SquareLayout.java
+++ app/src/main/java/com/doors/photos/SquareLayout.java
@@ -2,23 +2,27 @@ package com.doors.photos;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 /**
  * Created by domin on 8/1/2018.
  */
 
-public class SquareLayout extends RelativeLayout {
+public class SquareLayout extends LinearLayout {
     public SquareLayout(Context context) {
         super(context);
+        this.setOrientation(LinearLayout.VERTICAL);
     }
 
     public SquareLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.setOrientation(LinearLayout.VERTICAL);
     }
 
     public SquareLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.setOrientation(LinearLayout.VERTICAL);
     }
     @Override
     public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
