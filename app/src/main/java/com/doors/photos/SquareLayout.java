529c20488b4236b5d8d13d9fb8d4cc32ded763c5
Domi <domi04151309business@gmail.com>
2018-08-01 20:46:21 +0200

    Initial commit

---
+++ app/src/main/java/com/doors/photos/SquareLayout.java
@@ -0,0 +1,27 @@
+package com.doors.photos;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by domin on 8/1/2018.
+ */
+
+public class SquareLayout extends RelativeLayout {
+    public SquareLayout(Context context) {
+        super(context);
+    }
+
+    public SquareLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec); // This is the key that will make the height equivalent to its width
+    }
+}
